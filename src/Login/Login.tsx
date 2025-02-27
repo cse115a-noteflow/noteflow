b914c34789a4c0eb7e92c520ae5eaf5a7e3f85ff
cabalex <31020729+cabalex@users.noreply.github.com>
2025-02-27 04:55:05 -0800

    Linting

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -7,35 +7,35 @@ import GoogleIcon from '@mui/icons-material/Google';
 function Login({ api }: { api: API }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const toggleExpansion = () => {
-    setIsExpanded(prevState => !prevState); // Toggle the state
+    setIsExpanded((prevState) => !prevState); // Toggle the state
   };
   return (
-    <div className = "container">
+    <div className="container">
       <div className="left">
-        <div className = "logo">
-          NoteFlow
-        </div>
+        <div className="logo">NoteFlow</div>
         <div className="title-cont-outer">
           <h2>Note-taking,</h2>
-          <div className="title-cont-inner">
-            Enhanced.
+          <div className="title-cont-inner">Enhanced.</div>
+          <div className="info-icon">
+            <ExpandMoreIcon onClick={toggleExpansion} />
           </div>
-          <div className="info-icon"><ExpandMoreIcon onClick={toggleExpansion}/></div>
-          <div className={`info-box ${isExpanded ? 'expanded' : ''}`}> 
-          NoteFlow is note-taking application that integrates Retrieval Augmented
-          Generation (RAG) to retrieve information from notes faster. Notes are LLM-enhanced 
-          for quick summarization and flashcard generation.
+          <div className={`info-box ${isExpanded ? 'expanded' : ''}`}>
+            NoteFlow is note-taking application that integrates Retrieval Augmented Generation (RAG)
+            to retrieve information from notes faster. Notes are LLM-enhanced for quick
+            summarization and flashcard generation.
           </div>
         </div>
       </div>
       <div className="right">
         <div className="login">
-        <p>Continue with</p>
-        <button onClick={() => api.signInWithGoogle()}><GoogleIcon className="g-icon" />Sign in with Google</button>
+          <h3>Sign in to start writing better notes.</h3>
+          <button onClick={() => api.signInWithGoogle()}>
+            <GoogleIcon className="g-icon" />
+            Sign in with Google
+          </button>
+        </div>
       </div>
     </div>
-    </div>
-    
   );
 }
 
